2d5ac35af06241f1ba982f75334fe51eefddb61f
van_mit <ivan.mitskovets@gmail.com>
2015-10-01 02:10:53 +0300

    tmp

--- classes.cpp
+++ classes.cpp
@@ -1,7 +1,9 @@
 #include <vector>
-//lev.krainov@gmail.com
 #include <string>
 #include <iostream>
+#include <fstream>
+//lev.krainov@gmail.com
+
 class MyMatrix{
 	int _dim;
 	std::vector<std::vector<float>> vecvec;
@@ -29,10 +31,11 @@ class MyMatrix{
 		return (unsigned long) _dim;
 	}
 	float get(int y, int x) const{
-		return vecvec[y][x];
+		float tmp = vecvec[y][x];
+		return tmp;
 	}
-	void put(int y, int x, float mdata){
-		vecvec[y][x] = mdata;
+	void put(int y, int x, float data){
+		vecvec[y][x] = data;
 	}
 	void operator+=(const MyMatrix &b){
 		for(int i = 0; i < _dim; i++){
@@ -71,5 +74,62 @@ class MyMatrix{
 };
 
 int main(){
+	std::ifstream inputA;
+	std::ifstream inputB;
+	std::ifstream inputDim;
+	inputA.open("matrixA.txt");
+	inputB.open("matrixB.txt");
+	inputDim.open("dim.txt");
+	std::vector<float> vec((unsigned long) 5,0);
+	float tmp = 0;
+	std::string tmps;
+	int dim;
+	inputDim >> dim;
+	MyMatrix A(dim);
+	MyMatrix B(dim);
+	MyMatrix C(true,dim);
+	for (int i = 0; i < dim; i++){
+		for (int j = 0; j < dim; j++){
+			inputA >> tmps;
+			tmp = std::stof(tmps);
+			A.put(j, i, tmp);
+		}
+	}
+	for (int i = 0; i < dim; i++){
+		for (int j = 0; j < dim; j++){
+			inputB >> tmps;
+			tmp = std::stof(tmps);
+			B.put(j, i, tmp);
+		}
+	}
+	std::cout << "Matrix A:" << std::endl;
+	for (int i = 0; i < dim; i++){
+		for (int j = 0; j < dim; j++){
+			tmp = A.get(j,i);
+			std::cout << tmp << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "Matrix B:" << std::endl;
+	for (int i = 0; i < dim; i++){
+		for (int j = 0; j < dim; j++){
+			tmp = B.get(j,i);
+			std::cout << tmp << " ";
+		}
+		std::cout << std::endl;
+	}
+	B *= A;
+	std::cout << "Matrix B*=A:" << std::endl;
+	for (int i = 0; i < dim; i++){
+		for (int j = 0; j < dim; j++){
+			tmp = B.get(j,i);
+			std::cout << tmp << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << B.get(0,1);
+	inputA.close();
+	inputB.close();
+	inputDim.close();
 	
 }
