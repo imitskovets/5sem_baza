ba1e7cab65287f81c93f23df30b1b068851d7f50
van_mit <ivan.mitskovets@gmail.com>
2015-10-01 03:24:22 +0300

    -1 T -

--- classes.cpp
+++ classes.cpp
@@ -4,6 +4,23 @@
 #include <fstream>
 //lev.krainov@gmail.com
 
+float vectorMultiply(std::vector<float> a,std::vector<float> b){
+	// a * b
+	float c = 0.0;
+	int dim = (int) a.size();
+	for (int i = 0; i < dim; i++){
+		c += a[i] * b[i];
+	}
+	return c;
+}
+std::vector<float> vectorSubtraction(std::vector<float> a, std::vector<float> b){	// return a-b
+	std::vector<float> c((unsigned long) a.size(),0);
+	for (int i = 0; i < (int)a.size() ; i++){
+		c[i] = a[i] - b[i];
+	}
+	return c;
+}
+
 class MyMatrix{
 	int _dim;
 	std::vector<std::vector<float>> vecvec;
@@ -54,18 +71,32 @@ class MyMatrix{
 		}
 	}
 	void operator*=(const MyMatrix &b){
-		int tmp = 0;
-		std::vector<float> temporalVector((unsigned long) _dim,0);
+		std::vector<float> temporalVectorA((unsigned long) _dim,0);
+		std::vector<float> temporalVectorB((unsigned long) _dim,0);
 		for(int k = 0; k < _dim; k++){
 			for(int l = 0; l < _dim; l++){
-				temporalVector[l] = vecvec[k][l];
+				//temporalVectorA[l] = vecvec[k][l];
+				temporalVectorA = vecvec[k];
 			}
 			for(int i = 0; i < _dim; i++){
-				for(int j = 0; j < _dim; j++){
-					tmp += temporalVector[j] + b.get(j, i);
+				for(int l = 0; l < _dim; l++){
+					temporalVectorB[l] = b.get(l, i);
 				}
-				vecvec[k][i]=tmp;
-				tmp=0;
+				vecvec[k][i] = vectorMultiply(temporalVectorA, temporalVectorB);
+			}
+		}
+	}
+	void operator/(const MyMatrix &b){	// transpose
+		for(int i = 0; i < _dim; i++){
+			for(int j = 0; j < _dim; j++){
+				vecvec[i][j] = b.get(j,i);
+			}
+		}
+	}
+	void operator^(const MyMatrix &b){	// ^(-1)	vecvec must be E
+		for(int i = 0; i < _dim; i++){
+			for(int j = 0; j < _dim; j++){
+				vecvec[i][j] = b.get(j,i);
 			}
 		}
 	}
@@ -80,7 +111,6 @@ int main(){
 	inputA.open("matrixA.txt");
 	inputB.open("matrixB.txt");
 	inputDim.open("dim.txt");
-	std::vector<float> vec((unsigned long) 5,0);
 	float tmp = 0;
 	std::string tmps;
 	int dim;
@@ -88,6 +118,7 @@ int main(){
 	MyMatrix A(dim);
 	MyMatrix B(dim);
 	MyMatrix C(true,dim);
+	// input begin
 	for (int i = 0; i < dim; i++){
 		for (int j = 0; j < dim; j++){
 			inputA >> tmps;
@@ -102,6 +133,7 @@ int main(){
 			B.put(j, i, tmp);
 		}
 	}
+	// input end
 	std::cout << "Matrix A:" << std::endl;
 	for (int i = 0; i < dim; i++){
 		for (int j = 0; j < dim; j++){
@@ -118,16 +150,15 @@ int main(){
 		}
 		std::cout << std::endl;
 	}
-	B *= A;
-	std::cout << "Matrix B*=A:" << std::endl;
+	A / B;
+	std::cout << "Matrix A/B:" << std::endl;
 	for (int i = 0; i < dim; i++){
 		for (int j = 0; j < dim; j++){
-			tmp = B.get(j,i);
+			tmp = A.get(j,i);
 			std::cout << tmp << " ";
 		}
 		std::cout << std::endl;
 	}
-	std::cout << B.get(0,1);
 	inputA.close();
 	inputB.close();
 	inputDim.close();
