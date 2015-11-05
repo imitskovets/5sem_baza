cb5dd026c9934cf5631639b9de5e74ca562804ed
van_mit <ivan.mitskovets@gmail.com>
2015-11-05 04:30:48 +0300

    sem1 final

--- labs/sem1.cpp
+++ labs/sem1.cpp
@@ -8,6 +8,9 @@
 double myFunc(double x, double y, double h, double a, double b){
     return  ((1 + a * h) * y + b * h * x);
 }
+double myFunc2(double x, double y1, double y2, double h, double a, double b){
+    return  (2 * h * (a * y1 + b * x ) + y2);
+}
 double func(double x, double h, double a, double b){
     return ((1 + b / pow(a, 2)) * exp(a * x) - (b * x) / a - b / a * a);
 }
@@ -45,21 +48,29 @@ int main(){
     double X;
     double Y;
     double myY;
+    double myY2;
     double errorY;
-    double tmp = 0;
+    double tmp1 = 0;
+    double tmp2 = 0;
     myY = 1;
+    myY2 = 1;
     Y = 1;
     X = x_start;
     FILE* fd1 = fopen("myY.txt","w");
     FILE* fd2 = fopen("Y.txt","w");
+    FILE* fd3 = fopen("myY2.txt","w");
     fprintf(fd1,"%f\t%f\n", X, myY);
     fprintf(fd2,"%f\t%f\n", X, Y);
+    fprintf(fd3,"%f\t%f\n", X, myY2);
     for (long int i = 0; i < N; i++ ){
         X += h;
-        tmp = myY;
-        myY = myFunc(X,tmp,h,a,b);
+        tmp2 = tmp1;
+        tmp1 = myY;
+        myY = myFunc(X,tmp1,h,a,b);
+        myY2 = myFunc2(X,tmp1,tmp2,h,a,b);
         Y = func(X,h,a,b);
         fprintf(fd1,"%f\t%f\n", X, myY);
+        fprintf(fd3,"%f\t%f\n", X, myY2);
         fprintf(fd2,"%f\t%f\n", X, Y);
         errorY += h * pow((Y - myY), 2);
     }
